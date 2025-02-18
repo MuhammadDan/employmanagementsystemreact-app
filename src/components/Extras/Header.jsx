850b2335938958028ad33595c0ba5f9a9b7d14d7
DanKhan1 <chaudriali034@gmail.com>
2025-02-19 01:04:17 +0500

    Eleventh comit: header bhi sahi karliya hai

--- src/components/Extras/Header.jsx
+++ src/components/Extras/Header.jsx
@@ -15,7 +15,7 @@ const Header = (props) => {
   return (
     <>
     <div className='flex items-end justify-between'>
-        <h1 className='text-xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>Username 👋</span></h1>
+        <h1 className='text-xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>{props.UserLoggedInData?.Firstname || "Admin"}👋</span></h1>
         <button onClick={logoutuser} className='bg-red-600 text-white text-lg font-medium px-5 py-2 rounded-md'>Log Out</button>
     </div>
     </>
